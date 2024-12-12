25e58e4361e8a0ba7166312e7bd4d97b0d57a6d8
pier2003 <pigidanti@gmail.com>
2024-12-12 18:57:12 +0100

    disciplinary report student gui

--- src/main/java/gui/DisciplinaryReportStudentSceneManager.java
+++ src/main/java/gui/DisciplinaryReportStudentSceneManager.java
@@ -1,33 +1,88 @@
 package gui;
 
 import java.io.IOException;
+import java.util.Iterator;
 
+import DaoExceptions.DaoConnectionException;
+import DaoExceptions.DisciplinaryReportException;
+import DaoExceptions.StudentDaoException;
 import businessLogic.StudentController;
+import domainModel.DisciplinaryReport;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class DisciplinaryReportStudentSceneManager {
-	@FXML
-	private Button back;
-	private Stage stage;
-	private Scene scene;
-	private Parent root;
-	private static StudentController studentController;
 
-	public void switchToStudentScene() throws IOException {
-		root = FXMLLoader.load(getClass().getResource("../resources/StudentInterface.fxml"));
-		stage = (Stage) back.getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
+    @FXML
+    private Button back;
 
-	public static void setController(StudentController studentController) {
-		DisciplinaryReportStudentSceneManager.studentController = studentController;
-	}
+    @FXML
+    private TableView<DisciplinaryReport> tableView;
 
+    @FXML
+    private TableColumn<DisciplinaryReport, String> dateColumn;
+
+    @FXML
+    private TableColumn<DisciplinaryReport, String> teacherColumn;
+
+    @FXML
+    private TableColumn<DisciplinaryReport, String> descriptionColumn;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private static StudentController studentController;
+
+    @FXML
+    public void initialize() {
+    	dateColumn.setCellValueFactory(new PropertyValueFactory<>("Date"));
+    	teacherColumn.setCellValueFactory(cellData -> new SimpleStringProperty(
+                cellData.getValue().getTeacher().getSurname() + " " + cellData.getValue().getTeacher().getName()
+    	        ));
+    	descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("Description"));
+
+        populateTableView();
+    }
+
+    public void switchToStudentScene() throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../resources/StudentInterface.fxml"));
+        stage = (Stage) back.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void setController(StudentController studentController) {
+        DisciplinaryReportStudentSceneManager.studentController = studentController;
+    }
+
+    private void populateTableView() {
+        if (studentController == null) {
+            throw new IllegalStateException("StudentController non inizializzato");
+        }
+
+        Iterator<DisciplinaryReport> reportsIterator = null;
+		try {
+			reportsIterator = studentController.getDisciplinaryReports();
+		} catch (DisciplinaryReportException | DaoConnectionException | StudentDaoException e) {
+			e.printStackTrace();
+		}
+
+        ObservableList<DisciplinaryReport> reportsList = FXCollections.observableArrayList();
+        while (reportsIterator.hasNext()) {
+            reportsList.add(reportsIterator.next());
+        }
+
+        tableView.setItems(reportsList);
+    }
 }
