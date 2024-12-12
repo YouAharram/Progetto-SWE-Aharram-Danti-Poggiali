84e124d58f6a871d15ad2c2e4675126230dd3dcc
YouAharram <you.aharram@gmail.com>
2024-12-12 16:21:27 +0100

    finito cambio scene su teacher

---
+++ src/main/java/gui/MeetingTeacherManager.java
@@ -0,0 +1,27 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MeetingTeacherManager {
+	@FXML
+	private Button btnBack;
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void backAtTeacherScene() throws IOException {
+		root = FXMLLoader.load(getClass().getResource("../resources/TeacherInterface.fxml"));
+		stage = (Stage) btnBack.getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
