080df1e1ebd8e2941c0f640704937065485f4838
YouAharram <you.aharram@gmail.com>
2024-12-14 16:54:03 +0100

    teacherController

--- src/main/java/gui/InterfaceTeacherManager.java
+++ src/main/java/gui/InterfaceTeacherManager.java
@@ -4,20 +4,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Spliterator;
@@ -39,7 +41,7 @@ import domainModel.TeachingAssignment;
 
 public class InterfaceTeacherManager {
 
-	private static TeacherController teacherController;
+	static TeacherController teacherController;
 
 	@FXML
 	private Button btnLesson;
@@ -50,21 +52,24 @@ public class InterfaceTeacherManager {
 	@FXML
 	private ChoiceBox<String> cbClasses;
 	@FXML
-	private TableView<String> tvGradesStudents = new TableView<>();
+	private TableView<ObservableList<String>> tvGradesStudents;
+	ObservableList<ObservableList<String>> rows = FXCollections.observableArrayList();
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	private int idTeaching;
+	private int numberOfStudents = 0;
 
 	@FXML
-	public void initialize() {
+	public void initialize() throws StudentDaoException, DaoConnectionException, SchoolClassDaoException,
+			GradeDaoException, TeachingAssignmentDaoException {
+		setControllerForAllScene();
 		cbTeachings.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
 			if (newValue != null) {
 				handleChoiceBoxChange(newValue);
 			}
 		});
 		populateTeachings();
-		ConfigureColumn();
 
 	}
 
@@ -107,35 +112,139 @@ public class InterfaceTeacherManager {
 			}
 		}
 	}
-	
+
 	@FXML
-	public void showGrades() throws StudentDaoException, DaoConnectionException, SchoolClassDaoException, GradeDaoException, TeachingAssignmentDaoException {
+	public void showGrades() throws StudentDaoException, DaoConnectionException, SchoolClassDaoException,
+			GradeDaoException, TeachingAssignmentDaoException {
+		ConfigureColumnForGrade();
 		Iterator<Student> students = teacherController.getStudentsByClass(new SchoolClass(cbClasses.getValue()));
-		while(students.hasNext()) {
-			teacherController.getAllStudentGradesByTeaching(students.next(), new TeachingAssignment(cbTeachings.getSelectionModel().getSelectedIndex(), cbTeachings.getValue(), teacherController.getTeacher(), new SchoolClass(cbClasses.getValue())));
+
+		int rowindex = 0;
+		while (students.hasNext()) {
+			int c = 1;// Ad esempio, 5 studenti
+			Student student = students.next();
+			System.out.println(student.getName());
+			ObservableList<String> row = rows.get(rowindex);
+			rowindex++;
+			row.set(0, String.valueOf(student.getName() + " " + student.getSurname()));
+			Iterator<Grade> gradesPerStudent = teacherController.getAllStudentGradesByTeaching(student,
+					new TeachingAssignment(cbTeachings.getSelectionModel().getSelectedIndex() + 1,
+							cbTeachings.getValue(), teacherController.getTeacher(),
+							new SchoolClass(cbClasses.getValue())));
+			while (gradesPerStudent.hasNext()) { // Ad esempio, 10 voti per studente
+				row.set(c, String.valueOf(gradesPerStudent.next().getValue()));
+				c++;
+			}
 		}
 	}
-	
-	private void ConfigureColumn() {
-		int size = 15;
-		TableColumn<String, String> student = new TableColumn<String, String>("Student");
-		tvGradesStudents.getColumns().add(student);
-		for(int i=0; i < size ; i++) {
-			String columnName = "Grade "+(i+1)+"°";
-			TableColumn<String, String> column = new TableColumn<String, String>(columnName);
-			//column.setCellValueFactory(cellData -> cellData.getValue().getda);
+
+	private void ConfigureColumnForGrade() throws StudentDaoException, DaoConnectionException, SchoolClassDaoException,
+			GradeDaoException, TeachingAssignmentDaoException {
+		// Svuota i dati del modello
+		tvGradesStudents.getItems().clear();
+		// Rimuove tutte le colonne
+		tvGradesStudents.getColumns().clear();
+
+		int size = getMaxNumberOfGrades() + 1; // 10 colonne (per esempio, 10 voti)
+
+		for (int i = 0; i < size; i++) {
+			TableColumn<ObservableList<String>, String> column;
+			if (i == 0) {
+				column = new TableColumn<>("Student: ");
+			} else {
+				column = new TableColumn<>("Grade " + (i));
+			}
+
+			final int colIndex = i; // Indice di colonna
+
+			// Imposta la cella come elemento della lista corrispondente all'indice
+			column.setCellValueFactory(
+					cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().get(colIndex)));
+
+			// Rendi la cella modificabile
+			column.setCellFactory(TextFieldTableCell.forTableColumn());
+			column.setOnEditCommit(event -> event.getRowValue().set(colIndex, event.getNewValue()));
 			tvGradesStudents.getColumns().add(column);
 		}
-		tvGradesStudents.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
-		ScrollPane scrollPane = new ScrollPane(tvGradesStudents);
-        scrollPane.setFitToWidth(false); // Permette lo scorrimento orizzontale
-        scrollPane.setFitToHeight(true);
+		System.out.println(numberOfStudents);
+		for (int r = 0; r < numberOfStudents; r++) {
+			ObservableList<String> row = FXCollections.observableArrayList();
+			for (int c = 0; c < size; c++) {
+				row.add("");
+			}
+			rows.add(row);
+		}
+		tvGradesStudents.setItems(rows);
+	}
 
+	public void showStudents() throws StudentDaoException, DaoConnectionException, SchoolClassDaoException {
+		// Svuota i dati del modello
+		tvGradesStudents.getItems().clear();
+		// Rimuove tutte le colonne
+		tvGradesStudents.getColumns().clear();
+
+		// Crea le colonne
+		TableColumn<ObservableList<String>, String> column1 = new TableColumn<>("Number");
+		TableColumn<ObservableList<String>, String> column2 = new TableColumn<>("Name");
+		TableColumn<ObservableList<String>, String> column3 = new TableColumn<>("Surname");
+
+		// Imposta il cell value factory per ogni colonna
+		column1.setCellValueFactory(data -> 
+		    new SimpleStringProperty(data.getValue().get(0))); // Colonna "Number"
+		column2.setCellValueFactory(data -> 
+		    new SimpleStringProperty(data.getValue().get(1))); // Colonna "Name"
+		column3.setCellValueFactory(data -> 
+		    new SimpleStringProperty(data.getValue().get(2))); // Colonna "Surname"
+
+		// Aggiunge le colonne alla TableView
+		tvGradesStudents.getColumns().add(column1);
+		tvGradesStudents.getColumns().add(column2);
+		tvGradesStudents.getColumns().add(column3);
+
+		// Ottieni gli studenti dalla classe selezionata
+		Iterator<Student> students = teacherController.getStudentsByClass(new SchoolClass(cbClasses.getValue()));
+		ObservableList<ObservableList<String>> newRows = FXCollections.observableArrayList();
+		int i = 1;
+
+		// Popola i dati
+		while (students.hasNext()) {
+		    Student student = students.next();
+		    ObservableList<String> row = FXCollections.observableArrayList();
+		    row.add(String.valueOf(i));  // Numero
+		    row.add(student.getName()); // Nome
+		    row.add(student.getSurname()); // Cognome
+		    newRows.add(row);
+		    i++;
+		}
+
+		// Imposta i dati nella TableView
+		tvGradesStudents.setItems(newRows);
+	}
+
+	private int getMaxNumberOfGrades() throws StudentDaoException, DaoConnectionException, SchoolClassDaoException,
+			GradeDaoException, TeachingAssignmentDaoException {
+		Iterator<Student> students = teacherController.getStudentsByClass(new SchoolClass(cbClasses.getValue()));
+		int maxNumberOfGrades = 0;
+		while (students.hasNext()) {
+			numberOfStudents++;
+			Iterator<Grade> gradesPerStudent = teacherController.getAllStudentGradesByTeaching(students.next(),
+					new TeachingAssignment(cbTeachings.getSelectionModel().getSelectedIndex() + 1,
+							cbTeachings.getValue(), teacherController.getTeacher(),
+							new SchoolClass(cbClasses.getValue())));
+			List<Grade> grades = new ArrayList<Grade>();
+			gradesPerStudent.forEachRemaining(grades::add);
+			if (grades.size() > maxNumberOfGrades) {
+				maxNumberOfGrades = grades.size();
+			}
+		}
+		System.out.println("max: " + maxNumberOfGrades);
+		return maxNumberOfGrades;
 	}
 
 	@FXML
 	public void openLesson() throws IOException {
 		openWindow("../resources/LessonInterface.fxml", "Lesson");
+		LessonTeacherManager.setClass(new SchoolClass(cbClasses.getValue()));
 	}
 
 	@FXML
@@ -147,6 +256,11 @@ public class InterfaceTeacherManager {
 	public void openMeeting() throws IOException {
 		openWindow("../resources/MeetingInterface.fxml", "Meeting");
 	}
+	
+	@FXML
+	public void openGrades() throws IOException {
+		openWindow("../resources/GradesTeacherInterface.fxml","Grades");
+	}
 
 	private void openWindow(String path, String nameWindow) throws IOException {
 		root = FXMLLoader.load(getClass().getResource(path));
@@ -157,6 +271,9 @@ public class InterfaceTeacherManager {
 		stage.show();
 	}
 
+	private void setControllerForAllScene() {
+		LessonTeacherManager.setController(teacherController);
+	}
 	public static void setController(TeacherController teacherController) {
 		InterfaceTeacherManager.teacherController = teacherController;
 	}
--- src/main/java/gui/LessonTeacherManager.java
+++ src/main/java/gui/LessonTeacherManager.java
@@ -1,21 +1,64 @@
 package gui;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import DaoExceptions.DaoConnectionException;
+import DaoExceptions.HomeworkDaoException;
+import DaoExceptions.LessonDaoException;
+import DaoExceptions.SchoolClassDaoException;
+import businessLogic.TeacherController;
+import domainModel.Homework;
+import domainModel.Lesson;
+import domainModel.SchoolClass;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class LessonTeacherManager {
-
+	
+	
 	@FXML
 	private Button btnBack;
+	@FXML
+	private DatePicker datePicker;
+	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	
+	private static TeacherController teacherController;
+	private static SchoolClass schoolClass;
+	
+    @FXML
+    private TableView<Lesson> lessonsTableView;
+
+    @FXML
+    private TableColumn<Lesson, String> subjectLessonColumn;
+    
+    @FXML
+    private TableColumn<Lesson, String> teacherLessonColumn;
+
+    @FXML
+    private TableColumn<Lesson, String> startHourColumn;
+
+    @FXML
+    private TableColumn<Lesson, String> endHourColumn;
+
+    @FXML
+    private TableColumn<Lesson, String> descriptionLessonColumn;
 	
 	public void backAtTeacherScene() throws IOException {
 		root = FXMLLoader.load(getClass().getResource("../resources/TeacherInterface.fxml"));
@@ -25,4 +68,40 @@ public class LessonTeacherManager {
 		stage.show();
 	}
 	
+	public void showLesson() throws DaoConnectionException, LessonDaoException, SchoolClassDaoException {
+        System.out.println("dentro");
+		if (datePicker.getValue() == null) {
+            return;
+        }
+
+        LocalDate selectedDate = datePicker.getValue();
+
+        Iterator<Lesson> lessonsIterator = null;
+		try {
+			lessonsIterator = teacherController.getClassLessonsInDay(selectedDate,schoolClass);
+		} catch (DaoConnectionException | LessonDaoException |  SchoolClassDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        List<Lesson> lessons = new ArrayList<>();
+        lessonsIterator.forEachRemaining(lessons::add);
+        ObservableList<Lesson> lessonsList = FXCollections.observableArrayList(lessons);
+        lessonsTableView.setItems(lessonsList);
+
+        subjectLessonColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTeaching().getSubject()));
+        teacherLessonColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTeaching().getTeacher().getSurname() + " " + cellData.getValue().getTeaching().getTeacher().getName()));
+        startHourColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStartHour().toString()));
+        endHourColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEndHour().toString()));
+        descriptionLessonColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDescription()));
+	}
+	
+	public static void setController(TeacherController teacherController) {
+		LessonTeacherManager.teacherController = teacherController;
+	}
+	
+	protected static void setClass(SchoolClass schoolClass) {
+		LessonTeacherManager.schoolClass = schoolClass;
+		
+	}
 }
